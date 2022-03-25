196e45da16251e3ce21ca3c7c665dffe5fb09d0b
galust.gabrielyan <galust.gabrielyan@codetime.am>
2022-03-26 01:15:44 +0400

    add task popup reducer

--- src/redux/columns/columns__action.js
+++ src/redux/columns/columns__action.js
@@ -1,14 +1,25 @@
 export const constantsColumn = {
-    ADD__COLUMN: 'ADD__COLUMN'
+    ADD__COLUMN: 'ADD__COLUMN',
+    ADD__TASK: 'ADD__TASK'
 }
 
 
-export const addColumn = (name,color)=>{
-    return({
-        type:constantsColumn.ADD__COLUMN,
-        payload:{
-            name:name,
-            color:color
+export const addColumn = (name, color) => {
+    return ({
+        type: constantsColumn.ADD__COLUMN,
+        payload: {
+            name: name,
+            color: color
+        }
+    })
+}
+export const addTask = (title, info, columnId) => {
+    return ({
+        type: constantsColumn.ADD__TASK,
+        payload: {
+            title: title,
+            info: info,
+            columnId: columnId
         }
     })
 }
